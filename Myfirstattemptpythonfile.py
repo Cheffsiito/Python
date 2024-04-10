c39dade69b0fd01374503b832b021b0beb04b90f
Erubiel Cuevas <158236707+Cheffsiito@users.noreply.github.com>
2024-04-09 18:31:53 -0700

    Myfirstattemptpythonfile.py

--- Myfirstattemptpythonfile.py
+++ Myfirstattemptpythonfile.py
@@ -1,8 +1,7 @@
-# Captura de datos del usuario
-nombre = input("Ingrese su nombre: ")
-edad = input("Ingrese su edad: ")
-email = input("Ingrese su dirección de correo electrónico: ")
+nombre = input("\033[1;31mNombre: \033[0m")
+edad = input("\033[1;32mEdad: \033[0m")
+email = input("\033[1;34mEmail: \033[0m")
 
-print("Mi nombre es *{}*, tengo *{} años* y se enviará un correo electrónico a la siguiente dirección *{}*.".format(nombre, edad, email))
+mensaje = f"Mi nombre es \033[1;31m{nombre}\033[0m, tengo \033[1;32m{edad}\033[0m años y se enviará un correo electrónico a la siguiente dirección \033[1;34m{email}\033[0m."
+print(mensaje)
 
-input("Presione Enter para salir...")
