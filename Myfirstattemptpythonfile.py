057b5b7eefd8d92f101d409dbc5933a784f041d8
Erubiel Cuevas <158236707+Cheffsiito@users.noreply.github.com>
2024-04-09 18:01:07 -0700

    Add files via upload

---
+++ Myfirstattemptpythonfile.py
@@ -0,0 +1,8 @@
+# Captura de datos del usuario
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+email = input("Ingrese su dirección de correo electrónico: ")
+
+print("Mi nombre es *{}*, tengo *{} años* y se enviará un correo electrónico a la siguiente dirección *{}*.".format(nombre, edad, email))
+
+input("Presione Enter para salir...")
